c47a9f4b52cb6cf4d4916af70fc1bef7fe19dd5c
LimeDays <110297437+LimeDays@users.noreply.github.com>
2023-03-01 20:16:05 +0200

    Update List.java

--- FirstPracticeWithGenerics/List.java
+++ FirstPracticeWithGenerics/List.java
@@ -2,11 +2,11 @@ package org.itstep;
 
 import java.util.Arrays;
 
-// FIXME: обобщенный тип должен быть классом расширяющим интерфейс Comparable<T>
+
 class List<T extends Comparable<T>> {
     private T[] arrays;
     public List() {
-        // FIXME: создать массив типов Comparable
+        
         this.arrays = (T[]) new Comparable[0];
 
     }
@@ -22,7 +22,7 @@ class List<T extends Comparable<T>> {
 
     public T min() {
         T min = arrays[0];
-        // FIXME: реализовать поиск наименьшего элемента используя метод compareTo интерфейса Comparable<T>
+       
         for (int i = 1; i < arrays.length; i++) {
             if (min.compareTo(arrays[i]) > 0) {
                 min = arrays[i];
@@ -35,7 +35,7 @@ class List<T extends Comparable<T>> {
 
         public T max () {
             T max = arrays[0];
-            // FIXME: реализовать поиск наибольшего элемента используя метод compareTo интерфейса Comparable<T>
+           
 
             for (int i = 1; i < arrays.length; i++) {
                 if (max.compareTo(arrays[i]) < 0) {
